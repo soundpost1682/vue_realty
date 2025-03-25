0eea3501e82974acadb788c1aed9111d0e63bcac
evan <guarneri1682@gmail.com>
2025-03-24 23:58:42 -0700

    test02

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,24 @@
   <div class="menu">
     <a v-for="jak in menus" :key="jak">{{ jak }}</a>
   </div>
+  
   <div>
+    <img src="./assets/room0.jpg" alt="">
     <H4>{{ products[0] }}</H4>
     <p >{{ 500 }}</p>
-    <button @click="cnt++">Report Fake</button> <span>No.count : {{ cnt }}</span>
+    <button @click="cnt[0]++">Report Fake</button> <span>No.count : {{ cnt[0] }}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" alt="">
     <H4>{{ products[1] }}</H4>
     <p >{{ 500 }}</p>
-    
+    <button @click="cnt[1]++">Report Fake</button> <span>No.count : {{ cnt[1] }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" alt="">
     <H4>{{ products[2] }}</H4>
     <p >{{ 500 }}</p>
+    <button @click="cnt[2]++">Report Fake</button> <span>No.count : {{ cnt[2] }}</span>
   </div>
 </template>
 
@@ -24,10 +29,15 @@ export default {
   name : 'App',
   data(){
     return {
-      cnt : 0,
+      cnt : [0,0,0],
       menus : ['Home','Products','About'],
       products:['Vancouver','Surrey','Langley'],
       
+    }
+  },
+  methods:{
+    increase(){
+      this.cnt+=1
     }
   },
   components : {
