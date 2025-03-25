4db490a525eeeeb4dfdcc300a58d4498c03ca432
evan <guarneri1682@gmail.com>
2025-03-25 15:53:25 -0700

    Modals

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,43 @@
 <template>
+  
+  <div class="black-bg" v-if="modal_status==true">
+    <div class="white-bg">
+      <img :src="onerooms[select].image" style="width: 100%;">
+      <h4>{{onerooms[select].title}}</h4>
+      <p>{{ onerooms[select].content }}</p>
+      <p>{{ onerooms[select].price }}원</p>
+      <button @click="modal_status=false">Close</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="jak in menus" :key="jak">{{ jak }}</a>
   </div>
   
-  <div>
-    <img src="./assets/room0.jpg" alt="">
-    <H4>{{ products[0] }}</H4>
-    <p >{{ 500 }}</p>
-    <button @click="cnt[0]++">Report Fake</button> <span>No.count : {{ cnt[0] }}</span>
+  <div v-for="(oneroom,i) in onerooms" :key="i">
+    <img :src="onerooms[i].image" class="room-img">
+    <H4 @click="modal_status=true; select=i">{{ onerooms[i].title }}</H4>
+    <p >${{ onerooms[i].price }}</p>
+    
+    
   </div>
-  <div>
-    <img src="./assets/room1.jpg" alt="">
-    <H4>{{ products[1] }}</H4>
-    <p >{{ 500 }}</p>
-    <button @click="cnt[1]++">Report Fake</button> <span>No.count : {{ cnt[1] }}</span>
-  </div>
-  <div>
-    <img src="./assets/room2.jpg" alt="">
-    <H4>{{ products[2] }}</H4>
-    <p >{{ 500 }}</p>
-    <button @click="cnt[2]++">Report Fake</button> <span>No.count : {{ cnt[2] }}</span>
-  </div>
+  
 </template>
 
 <script>
+import data from './assets/oneroom.js'
+
 var arr = []
 export default {
   name : 'App',
   data(){
     return {
+      select : 0,
+      onerooms : data,
+      modal_status : false,
       cnt : [0,0,0],
       menus : ['Home','Products','About'],
       products:['Vancouver','Surrey','Langley'],
-      
     }
   },
   methods:{
@@ -46,6 +51,26 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 80%; height: 80%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px; 
+}
+.white-bg {
+  width: 80%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 50%;
+  margin-top: 40px;
+}
 .menu {
   background:darkslateblue;
   padding: 15px;
